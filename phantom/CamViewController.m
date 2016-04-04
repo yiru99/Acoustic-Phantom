ab379c15f91acf620503b8f938a0c8e89bb187e6
sheryllucky <luckyxinyun@gmail.com>
2016-04-03 22:09:14 -0400

    network communication, output stream

--- phantom/CamViewController.m
+++ phantom/CamViewController.m
@@ -6,21 +6,27 @@
 //  Copyright (c) 2016 Sheryl Zhang. All rights reserved.
 //
 
+
+// Project -> Build Phases -> Compile Sources
+// Ignore all warnings
+// Add '-w' to compiler flags
+
 #import "CamViewController.h"
 
 @interface CamViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
-@property (weak, nonatomic) IBOutlet UITextField *nameField;
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
+@property (nonatomic) IBOutlet UITextField *nameField;
+@property (nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic) IBOutlet UIToolbar *toolbar;
 
 @end
 
 @implementation CamViewController
 
+
 - (IBAction)takePicture:(id)sender {
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
-    //if device has a camera, take picture with camera
+    // if device has a camera, take picture with camera
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
     }
@@ -28,33 +34,154 @@
     else {
         imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     }
-    
-    //
+
     imagePicker.delegate = self;
     
     // place image picker on the screen
     [self presentViewController:imagePicker animated:YES completion:NULL];
 }
 
-//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-//{
-//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-//    if (self) {
-//        // Custom initialization
-//    }
-//    return self;
-//}
-//
-//- (void)viewDidLoad
-//{
-//    [super viewDidLoad];
-//    // Do any additional setup after loading the view from its nib.
-//}
-//
-//- (void)didReceiveMemoryWarning
-//{
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    
+    // Get picked image from info dictionary
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    
+    // Put that image onto the screen in our image view
+    self.imageView.image = image;
+    
+    // Save image to photo album
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    
+    // Take image picker off the screen -
+    // you must call this dismiss method
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    
+    
+    
+    [self initNetworkCommunication];
+    
+    [self sendString:@"Hello World!"];
+    
+    // NSLog(@"send string success");
+}
+
+
+- (void)initNetworkCommunication {
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"10.0.0.10", 1027, &readStream, &writeStream);
+    // NSInputStream *inputStream = ( NSInputStream *)readStream;
+    NSOutputStream *outputStream = (NSOutputStream *)writeStream;
+    // [inputStream setDelegate:(id<NSStreamDelegate>)self];
+    [outputStream setDelegate:self];
+    // [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    // [inputStream open];
+    [outputStream open];
+}
+
+
+// TODO:
+// fix this!
+- (void)sendString:(NSString *)string {
+    NSData *data = [[NSData alloc] initWithData:[string dataUsingEncoding:NSASCIIStringEncoding]];
+    NSLog(@"%@", [string dataUsingEncoding:NSUTF8StringEncoding]);
+    NSMutableData *_data = [[NSMutableData alloc] init];
+    [_data appendData:data];
+    [data release];
+}
+
+
+- (void)stream:(NSStream *)currentStream handleEvent:(NSStreamEvent)streamEvent {
+    //NSLog(@"stream event %u", streamEvent);
+    BOOL shouldClose = NO;
+    NSString *io;
+    NSString *event;
+    switch(streamEvent) {
+            
+        // error case?
+        case NSStreamEventEndEncountered:
+            shouldClose = YES;
+            NSLog(@"Closing stream...");
+            if(![currentStream hasBytesAvailable]) break;
+        // error case
+        case NSStreamEventErrorOccurred:
+            NSLog(@"Stream event ERROR");
+            break;
+        
+        // When bytes are available to receive
+        // Currently not useful
+        case NSStreamEventHasBytesAvailable: {
+            event = @"NSStreamEventHasBytesAvailable";
+            // NSMutableData *_data = [[NSMutableData alloc] init];
+            // uint8_t *readBytes = (uint8_t *)[_data mutableBytes];
+            // wtf?
+            // TODO
+            // something with _data
+            // readBytes += byteIndex; // instance variable byteIndex
+            // and byteIndex??
+            break;
+        }
+            
+        // Allocate buffer for output stream
+        case NSStreamEventHasSpaceAvailable:
+            event = @"NSStreamEventHasSpaceAvailable";
+            uint8_t buffer[64] = "ACPT";
+            int len;
+            len = [currentStream write:buffer maxLength:sizeof(buffer)];
+            if (len > 0) {
+                NSLog(@"YOOOOOO: sent");
+                [currentStream close];
+            }
+            break;
+        
+        case NSStreamEventNone:
+            NSLog(@"Stream event NONE");
+            shouldClose = YES;
+            break;
+        
+        // Open Stream
+        case NSStreamEventOpenCompleted:
+            NSLog(@"Stream event OPEN COMPLETED");
+            [self sendString:@"Hello World!"];
+            shouldClose = YES;
+            break;
+       
+        default:
+            NSLog(@"Unknown event: %@ : %lu", currentStream, streamEvent);
+            shouldClose = YES;
+            
+    }
+    
+    // TODO:
+    // output fails on this line
+    // if(shouldClose) [currentStream close];
+}
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.tabBarItem.title = @"Welcome";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
